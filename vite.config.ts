9e2e52c5776519deaa44f0e0f3e1e212fb70cfc6
Shoener <jshoene@amazon.com>
2025-12-20 18:38:13 -0500

    ci: GitHub Pages deploy + base path + SPA 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({
+export default defineConfig({`n  base: "/skateraded/",
   plugins: [react()],
   worker: { format: 'es' },
   server: {
